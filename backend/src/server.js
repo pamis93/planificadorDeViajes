71182833dbf382426be7ca8a03ae967691cbbd1d
Emanuel Gómez <vascogomez247@gmail.com>
2024-10-22 17:21:20 +0200

    middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,36 @@
 import express from 'express';
 import morgan from 'morgan';
 
+
+// Creamos el servidor.
 const server = express();
-server.use(morgan('dev'));
+
+// Middleware de peticiones entrantes.
+app.use(morgan('dev'));
+
+// Middleware conexión entre cliente y servidor.
+app.use(cors());
+
+// Middleware parseo del body en formato JSON
+app.use(express.json());
+
+// Middleware de manejo de errores.
+app.use((err, req, res, next) => {
+    
+  res.status(err.httpStatus || 500).send({
+      status: 'error',
+      message: err.message,
+  });
+
+});
+
+// Middleware 404 not found.
+
+app.use((req, res) => {
+  res.status(404).send({
+      status: 'error',
+      message: 'Ruta no encontrada',
+  });
+});
 
 export default server;
