300e868bf6c2a36610cc728068101377c480f056
Carlos Cue <cacp892@gmail.com>
2024-10-31 10:26:52 +0100

    fixed recoveryPasswordService and updateUserPassService

--- backend/src/services/users/recoverPasswordService.js
+++ backend/src/services/users/recoverPasswordService.js
@@ -2,26 +2,26 @@ import getPool from '../../db/getPool.js';
 import sendMailUtil from '../../utils/sendEmailUtil.js';
 
 const recoverPasswordService = async (email, recoverPassCode) => {
-    const pool = await getPool();
+  const pool = await getPool();
 
-    await pool.query(
-        `
-		UPDATE usuarios
+  await pool.query(
+    `
+		UPDATE users
         SET recoverPassCode = ?
         WHERE email = ?
 		`,
-        [recoverPassCode, email]
-    );
+    [recoverPassCode, email]
+  );
 
-    const subject = 'Recuperación de contraseña Planificador de Vuelos';
+  const subject = 'Recuperación de contraseña Planificador de Vuelos';
 
-    const body = `
+  const body = `
     <html>
         <body>
           <table bgcolor="#3c3c3c" width="670" border="0" cellspacing="0" cellpadding="0" align="left" > <tbody> <tr> <td align="left" style=" padding: 20px 40px; color: #fff; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; " > <p style=" margin: 10px 0 20px; font-size: 35px; font-weight: bold; color: #fff;" > <img src="https://img.freepik.com/vector-gratis/personaje-dibujos-animados-happy-airplane_1308-165793.jpg" alt="Logo" style="width: 40px; margin: 0 -3px -10px 0" /> Planificador de vuelos ✈️ </p> <p style="margin: 0 0 5px; font-size: 16px; color: #fff;"> Se ha solicitado la recuperación de la contraseña para el siguiente email </p> <p style="margin: 0 0 15px; font-size: 20px; color: #fff"> ${email} </p> <p style="margin: 35px 0 5px; font-size: 16px; color: #fff;"> Utilice el siguiente código de recuperación para crear una nueva contraseña: </p> <p style="margin: 0 0 15px; font-size: 20px; color: #fff"> ${recoverPassCode} </p> <p style="margin: 70px 0 2px; color: #fff;"> Gracias por volar con nosotros. </p> <p style="margin: 0 0 10px; color: #fff;">&copy; Planificador de vuelos ✈️ 2024</p> </td> </tr> </tbody> </table>
     </html>
 	`;
-    await sendMailUtil(email, subject, body);
+  await sendMailUtil(email, subject, body);
 };
 
 export default recoverPasswordService;
--- backend/src/services/users/updateUserPassService.js
+++ backend/src/services/users/updateUserPassService.js
@@ -1,26 +1,29 @@
 import bcrypt from 'bcrypt';
-import getPool from "../../db/getPool.js";
-import generateErrorsUtils from "../../utils/generateErrorsUtils.js";
-import selectUserByEmailService from "./selectUserByEmailService.js";
+import getPool from '../../db/getPool.js';
+import generateErrorsUtils from '../../utils/generateErrorsUtils.js';
+import selectUserByEmailService from './selectUserByEmailService.js';
 
-export const updateUserPassService = async(email, recoverPassCode, newPassword) => {
-    const pool = await getPool();
+export const updateUserPassService = async (
+  email,
+  recoverPassCode,
+  newPassword
+) => {
+  const pool = await getPool();
 
-    const user = await selectUserByEmailService(email);
+  const user = await selectUserByEmailService(email);
 
-    if(!user || user.recoverPassCode !== recoverPassCode){
-        throw generateErrorsUtils('Email o codigo de recuperación incorrecto', 409);
-    }
+  if (!user || user.recoverPassCode !== recoverPassCode) {
+    throw generateErrorsUtils('Email o codigo de recuperación incorrecto', 409);
+  }
 
-    const hashPassword = await bcrypt.hash(newPassword, 10);
+  const hashPassword = await bcrypt.hash(newPassword, 10);
 
-    await pool.query(
-        `
-            UPDATE usuarios
+  await pool.query(
+    `
+            UPDATE users
             SET password=?, recoverPassCode=null
             WHERE recoverPassCode=?
         `,
-        [hashPassword, recoverPassCode]
-    );
-}
- 
+    [hashPassword, recoverPassCode]
+  );
+};
