b62651e6962abbcf90d932cecaaf5a19ba6fe1b8
Lara Rodríguez <lararh393@gmail.com>
2024-11-01 21:17:16 +0100

    implementJoiAndMail

--- backend/src/services/users/insertUserService.js
+++ backend/src/services/users/insertUserService.js
@@ -1,17 +1,18 @@
 import bcrypt from 'bcrypt';
-import joi from 'joi';
+import Joi from 'joi';
 
 import getPool from '../../db/getPool.js';
 import generateErrorsUtils from '../../utils/generateErrorsUtils.js';
+import sendMailUtils from '../../utils/sendEmailUtil.js';
 
-const userSchema = joi.object({
-  email: joi.string().email().required(),
-  username: joi.string().alphanum().min(3).max(30).required(),
-  password: joi.string().min(8).required(),
-  name: joi.string().max(50).required(),
-  lastName: joi.string().max(50).required(),
-  avatar: joi.string().uri().optional(),
-  registrationCode: joi.string().max(100)
+const userSchema = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().min(8).required(),
+  name: Joi.string().max(50).required(),
+  lastName: Joi.string().max(50).required(),
+  avatar: Joi.string().uri().optional(),
+  registrationCode: Joi.string().max(100)
 });
 
 
@@ -40,6 +41,31 @@ export const insertUserService = async (
     if (userExists.length > 0) {
     throw generateErrorsUtils('El usuario ya está registrado', 409);
     }
+    //Creamos el asunto del email
+    const subject = 'Activación de tu cuenta de WonderFlifht';
+
+    //Creamos el cuerpo del email
+    const body = `
+       <html>
+          <body>
+            <h2>!!Bienvenid@ ${email}</h2>
+              <p>
+                Gracias por registrarte con nosotros. 
+                Estás a un paso de encontrar las mejores ofertas para tus futuros viajes e inolvidables aventuras. 
+                Activa tu cuenta
+                haciendo click en el siguiente enlace:
+              </p>
+              <p>
+                <a href="http://localhost:3001/users/validate/${registrationCode}">Activar Cuenta</a>
+                        
+                Ya Puedes empezar a disfrutar de nuestros servicios y 🛩️ por el mundo entero.
+              </p>
+          </body>
+      </html>
+        `;
+    //Llamamos a la función para enviar el email
+    await sendMailUtils(email, subject, body);
+
     // Hasheamos la contraseña.
     const hashedPass = await bcrypt.hash(password, 10);
 
--- backend/src/services/users/updateUserPassService.js
+++ backend/src/services/users/updateUserPassService.js
@@ -1,13 +1,27 @@
 import bcrypt from 'bcrypt';
+import Joi from 'joi';
+
 import getPool from '../../db/getPool.js';
 import generateErrorsUtils from '../../utils/generateErrorsUtils.js';
 import selectUserByEmailService from './selectUserByEmailService.js';
+
+const passSchema = Joi.object({
+    email:Joi.string().email().required(),
+    recoverPassCode: Joi.string().required(),
+    newPassword: Joi.string().min(8).required()
+  })
 
 export const updateUserPassService = async (
   email,
   recoverPassCode,
   newPassword
 ) => {
+ 
+  const { error } = passSchema.validate({email, recoverPassCode, newPassword});
+    if(error){
+      throw generateErrorsUtils(`Validation error: ${error.details[0].message}`, 400);
+    }   
+
   const pool = await getPool();
 
   const user = await selectUserByEmailService(email);
