d7f5153426f6ebdbefb0dd0e60f750a545e655f8
Carlos Cue <cacp892@gmail.com>
2024-11-10 20:56:43 +0100

    unificando rutaS en app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,15 @@
-// ! importacion para el router de emmanuel en espera de como se va a a trabajar
-// import Routes from './router/Routes';
-// ! si se usa esta hay que sacar Routes de la importación de abajo
-// import { Route, Routes } from "react-router-dom";
-
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
 // importaciones desde componentes
-import FlightSearch from "./components/FlightSearch/FlightSearch";
-import { Layout } from "./components/Layout/Layout";
-import PasswordRecovery from "./components/PasswordRecovery/PasswordRecovery";
-import Home from "./components/Home/Home";
-
-// importaciones desde pages
-// import { Layout } from "./pages/Layout";
-// import PasswordRecovery from "./pages/PasswordRecovery";
-// import Home from "./pages/Home";
+import FlightSearch from './components/FlightSearch/FlightSearch';
+import { Layout } from './components/Layout/Layout';
+import PasswordRecovery from './components/PasswordRecovery/PasswordRecovery';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 // css;
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
@@ -27,6 +19,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/recuperacion" element={<PasswordRecovery />} />
           <Route path="/search" element={<FlightSearch />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </>
@@ -34,16 +28,3 @@ function App() {
 }
 
 export default App;
-
-// ! Version con el router de emmanuel
-// import Routes from "./router/Routes";
-
-// function App() {
-//   return (
-//     <div>
-//       <Routes />
-//     </div>
-//   );
-// }
-
-// export default App;
