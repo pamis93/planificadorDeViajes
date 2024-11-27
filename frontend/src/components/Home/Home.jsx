6777cc062dd84e40d445225c7949fc7d4ac4349f
Gustavo Bolivar <gusta_bolivar@hotmail.com>
2024-11-27 22:47:12 +0100

    agrego imagenes para seccion fav en el home , cmabio el tamaño de flightsearch

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,5 +1,31 @@
+import FlightSearch from '../FlightSearch/FlightSearch'
+
 export default function Home() {
-  return (<div className="flex items-center justify-center h-screen w-screen bg-cover bg-center bg-[#9AA5BC] text-white">
-    HOLA SOY la pagina de HOME Voy a renderizar 
-  </div>); 
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-screen bg-cover bg-center bg-[#9AA5BC] text-white">
+      <FlightSearch className="w-full" />
+      <div className="max-w-4xl mx-auto mt-8 bg-[#686e9e] rounded-lg w-full">
+        <div className="relative p-8">
+          <h1 className=" text-3xl font-bold mb-4 ">EMPIEZA A PLANEAR TUS PRÓXIMAS AVENTURAS</h1>
+          <p className="mb-8">Conserva Tus Destinos Preferidos En Un Solo Lugar, Incluso Si Aún No Decides Reservar</p>
+          <div className="grid grid-cols-2 gap-8">
+            <div className="relative">
+              <img src="../../../public/paris.jpg" alt="Paris" className="rounded-lg mb-4 w-[350px] h-[400px]" />
+              <div className="absolute bottom-4 right-4 bg-[#9AA5BC] text-white px-4 py-2 rounded-md">
+                <p>PARIS</p>
+                <p>ida y vuelta desde 180€</p>
+              </div>
+            </div>
+            <div className="relative">
+              <img src="../../../public/londres.jpg" alt="Londres" className="rounded-lg mb-4 w-[350px] h-[400px]" />
+              <div className="absolute bottom-4 right-4 bg-[#9AA5BC] text-white px-4 py-2 rounded-md">
+                <p>LONDRES</p>
+                <p>ida y vuelta desde 120€</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
